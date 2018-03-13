c4b0f43b56e7e717bbe7479c28557e4012aab7c5
1172010644@qq.com <408542507@qq.com>
2018-03-13 15:51:17 +0800

    index.html

--- 01-calculate/calculate.js
+++ 01-calculate/calculate.js
@@ -6,6 +6,8 @@ var area=document.getElementById('are');
 var warn=document.getElementById('warn');
 var warnn=document.getElementById('warnn');
 
+
+var focus= document.getElementById('wid').focus();
 width.onblur=function(){
   if(width.value==""){
     warn.innerHTML="宽度不能为空";
@@ -13,8 +15,10 @@ width.onblur=function(){
 
  if(isNaN(width.value)==true){
        warn.innerHTML="信息格式不正确";
-  }
-  
+ }
+ if(width.value<0){
+   warn.innerHTML="宽度必须大于零";
+ } 
 }
 
 height.onblur=function(){
@@ -24,13 +28,15 @@ height.onblur=function(){
   if(isNaN(height.value)==true){
     warnn.innerHTML="信息格式不正确";
   }
+  if(height.value<0){
+    warnn.innerHTML="高度必须大于零";
+  }
 }
 
-btn.onclick=function (){
-      warn.innerHTML="";
-      warnn.innerHTML="";
+   btn.onclick=function (){
+     warn.innerHTML="";
+     warnn.innerHTML="";
       circum.value=(parseInt(width.value)+parseInt(height.value))*2;
       area.value=width.value*height.value;
- }
- 
+   } 
 
