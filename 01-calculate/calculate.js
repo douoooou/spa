150793df66ab2cc2989d50fe1639ebc01e6389d2
1172010644@qq.com <408542507@qq.com>
2018-03-13 16:37:53 +0800

    change

--- 01-calculate/calculate.js
+++ 01-calculate/calculate.js
@@ -11,32 +11,44 @@ var focus= document.getElementById('wid').focus();
 width.onblur=function(){
   if(width.value==""){
     warn.innerHTML="宽度不能为空";
+    width.focus();
   }
 
  if(isNaN(width.value)==true){
        warn.innerHTML="信息格式不正确";
+       width.focus();
  }
  if(width.value<0){
    warn.innerHTML="宽度必须大于零";
+   width.focus();
  } 
 }
 
 height.onblur=function(){
   if(height.value==""){
     warnn.innerHTML="高度不能为空";
+    height.focus();
   }
   if(isNaN(height.value)==true){
     warnn.innerHTML="信息格式不正确";
+    height.focus();
   }
   if(height.value<0){
     warnn.innerHTML="高度必须大于零";
+    height.focus();
   }
 }
 
-   btn.onclick=function (){
+function roundFractional(x, n) {
+        return Math.round(x * Math.pow(10, n)) / Math.pow(10, n);
+          
+}
+
+
+ btn.onclick=function (){
      warn.innerHTML="";
      warnn.innerHTML="";
-      circum.value=(parseInt(width.value)+parseInt(height.value))*2;
-      area.value=width.value*height.value;
+     circum.value=roundFractional((width.value)*2+(height.value)*2,1);
+      area.value=roundFractional(width.value*height.value,1);
    } 
 
