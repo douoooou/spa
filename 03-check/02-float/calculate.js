88ca199f8738417f4da06c9a35a911a64ed48731
1172010644@qq.com <408542507@qq.com>
2018-03-15 14:47:45 +0800

    check

---
+++ 03-check/02-float/calculate.js
@@ -0,0 +1,62 @@
+var width=document.getElementById('wid');
+var height=document.getElementById('hei');
+var btn=document.getElementById('btn');
+var circum=document.getElementById('cir');
+var area=document.getElementById('are');
+var warn=document.getElementById('warn');
+var warnn=document.getElementById('warnn');
+var focus= document.getElementById('wid').focus();
+
+width.keypress=function(e) {
+      validateKey(e);       
+};
+
+height.keypress=function(e) {
+      validateKey(e);
+        
+};
+//function validateKey(e) {
+//     if(/[abcdf-zABCDF-Z`~!@#$%^&*()=_+\[\]{}|;:'",<>/?\\]/.test(e.key)) e.preventDefault();"'`])
+//}
+
+
+width.onblur=function(){
+  if(width.value==""){
+    warn.innerHTML="宽度不能为空";
+    width.focus();
+  }else if(isNaN(width.value)==true){
+       warn.innerHTML="信息格式不正确";
+       width.focus();
+ }else if(width.value<0){
+   warn.innerHTML="宽度必须大于零";
+   width.focus();
+ } 
+}
+
+height.onblur=function(){
+  if(height.value==""){
+    warnn.innerHTML="高度不能为空";
+    height.focus();
+  }else if(isNaN(height.value)==true){
+    warnn.innerHTML="信息格式不正确";
+    height.focus();
+  }else if(height.value<0){
+    warnn.innerHTML="高度必须大于零";
+    height.focus();
+  }
+}
+
+function roundFractional(x, n) {
+        return Math.round(x * Math.pow(10, n)) / Math.pow(10, n);
+          
+}
+
+
+ btn.onclick=function (){
+      warn.innerHTML="";
+      warnn.innerHTML="";
+      circum.value=roundFractional((width.value)*2+(height.value)*2,1);
+      area.value=roundFractional(width.value*height.value,1);
+  
+} 
+
---
+++ 03-check/03-form/calculate.js
@@ -0,0 +1,42 @@
+var width=document.getElementById('wid');
+var height=document.getElementById('hei');
+var btn=document.getElementById('btn');
+var circum=document.getElementById('cir');
+var area=document.getElementById('are');
+var warn=document.getElementById('warn');
+var warnn=document.getElementById('warnn');
+
+function roundFractional(x, n) {
+        return Math.round(x * Math.pow(10, n)) / Math.pow(10, n);          
+}
+
+btn.onclick=function (){
+
+  if(width.value==""){
+    warn.innerHTML="宽度不能为空";
+  }else if(isNaN(width.value)==true){
+      warn.innerHTML="信息格式不正确";
+  }else if(width.value<0){
+      warn.innerHTML="宽度必须大于零";
+  }else{
+     warn.innerHTML="";
+  }
+   
+  
+  if(height.value==""){
+    warnn.innerHTML="高度不能为空";
+  }else if(isNaN(height.value)==true){
+      warnn.innerHTML="信息格式不正确";
+  }else if(height.value<0){
+      warnn.innerHTML="高度必须大于零";
+  }else{
+     warnn.innerHTML="";
+  } 
+
+   circum.value=roundFractional((width.value)*2+(height.value)*2,1);
+   area.value=roundFractional(width.value*height.value,1);
+}
+
+
+
+ 
